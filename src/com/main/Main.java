879a6778f36272d6e7253d9eef664cc1b1d23f06
aliak <wasjatka@gmail.com>
2018-05-05 23:59:05 +0300

    added print task name

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,13 +1,14 @@
 package com.main;
 
 import com.main.tasks.BaseTask;
-import com.main.tasks.sorts.Selection;
+import com.main.tasks.sorts.Bubble;
 
 import java.io.IOException;
 
 public class Main {
     public static final void main(String[] args) throws IOException {
-        BaseTask task = new Selection();
+        BaseTask task = new Bubble();
+        System.out.println("Task: " + task.getClass().getName());
         task.start();
     }
 }
