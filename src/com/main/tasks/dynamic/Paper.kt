7db342667601d9aad787535a4b6a9650ec57564c
Alexander Novik <alexander.novik@expcapital.com>
2022-04-15 11:49:16 +0300

    add paper

---
+++ src/com/main/tasks/dynamic/Paper.kt
@@ -0,0 +1,17 @@
+package com.main.tasks.dynamic
+
+import com.main.tasks.measure
+
+/*
+https://codeforces.com/problemset/problem/527/A
+A. Игра с бумагой
+*/
+
+fun main() {
+    measure { print(findCount(10, 7, 0)) }
+}
+
+fun findCount(w: Int, h: Int, count: Int): Int {
+    if (w == 0 || h == 0) return count
+    return findCount(h, w % h, count + w / h)
+}
