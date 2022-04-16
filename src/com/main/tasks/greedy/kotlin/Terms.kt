f8dd7e4a5ad8ae1d42152d15493f9d308be6296f
Alexander Novik <alexander.novik@expcapital.com>
2022-04-16 15:50:01 +0300

    add Terms

---
+++ src/com/main/tasks/greedy/kotlin/Terms.kt
@@ -0,0 +1,26 @@
+package com.main.tasks.greedy.kotlin
+
+import java.util.*
+
+/*
+Задача на программирование: различные слагаемые
+По данному числу n найдите максимальное число kk, для которого nn можно представить как сумму kk различных натуральных слагаемых. Выведите в первой строке число kk, во второй — kk слагаемых.
+*/
+fun main() {
+    val x = readLine()!!.toInt()
+    val numbers = Stack<Int>()
+    var currentTerm = 1
+    var sum = 0
+    while (sum < x) {
+        if (sum + currentTerm > x) { // last step
+            val delta = x - sum
+            numbers[numbers.size - 1] += delta
+            break
+        } else {                       // regular step
+            sum += currentTerm
+            numbers.add(currentTerm++)
+        }
+    }
+    println(numbers.size)
+    print(numbers.joinToString(" "))
+}
