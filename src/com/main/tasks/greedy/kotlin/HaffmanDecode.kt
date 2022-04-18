9f481a8ef71c919b62b0e81ebdd2c933b82dd5c7
Alexander Novik <alexander.novik@expcapital.com>
2022-04-18 12:05:26 +0300

    add HaffmanDecode

--- src/com/main/tasks/greedy/kotlin/HaffmanDecode.kt
+++ src/com/main/tasks/greedy/kotlin/HaffmanDecode.kt
@@ -1,8 +1,20 @@
 package com.main.tasks.greedy.kotlin
 
 fun main() {
-    val count = readLine()!!.take(1).toInt()
+    val (count, len) = readLine()!!.split(" ").map { it.toInt() }
+    val codes = mutableMapOf<String, String>()
     repeat(count) {
-
+        val (letter, code) = readLine()!!.split(": ").map { it.trim() }
+        codes[code] = letter
     }
+    var encodedStr = ""
+    readLine()!!.fold("") { encoded, letter ->
+        val nextCode = encoded + letter
+        if (codes.containsKey(nextCode)) {
+            encodedStr += codes[nextCode]; ""
+        } else {
+            nextCode
+        }
+    }
+    println(encodedStr)
 }
